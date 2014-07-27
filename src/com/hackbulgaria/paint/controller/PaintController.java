10f2f1d42f449e1af1744e61129c7b35c8fef36f
Milen <milengminev@gmail.com>
2014-07-27 22:58:46 +0300

    Added MouseListener

--- src/com/hackbulgaria/paint/controller/PaintController.java
+++ src/com/hackbulgaria/paint/controller/PaintController.java
@@ -1,5 +1,8 @@
 package com.hackbulgaria.paint.controller;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.JFrame;
 
 import com.hackbulgaria.paint.tools.Tool;
@@ -10,7 +13,9 @@ public class PaintController {
 	private final Tool currentTool;
 
 	public PaintController() {
+		MouseListener m = new MouseListener();
 		this.window = new PaintWindow();
+		window.addMouseListener(m);
 		window.setTitle("Paint!");
 		currentTool = Tool.DOT;
 		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -18,5 +23,14 @@ public class PaintController {
 		window.setFocusable(true);
 		window.setVisible(true);
 		//window.setSize(800, 600);
+	}
+	
+	
+	public class MouseListener extends MouseAdapter{
+		@Override
+		public void mousePressed(MouseEvent e) {
+			System.out.println("Click!");
+			super.mousePressed(e);
+		}
 	}
 }
