17e387ab59d491a9734f772666f69f848fa43fe4
rosen66 <rosen.iv.stoyanov@gmail.com>
2014-07-27 22:18:58 +0300

    initialising PaintControler and Tool

--- src/com/hackbulgaria/paint/Main.java
+++ src/com/hackbulgaria/paint/Main.java
@@ -1,9 +1,11 @@
 package com.hackbulgaria.paint;
 
+import com.hackbulgaria.paint.controller.PaintController;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		PaintController paintcontroller = new PaintController();
 	}
 
 }
--- src/com/hackbulgaria/paint/controller/PaintController.java
+++ src/com/hackbulgaria/paint/controller/PaintController.java
@@ -1,5 +1,20 @@
 package com.hackbulgaria.paint.controller;
 
-public class PaintController {
+import javax.swing.JFrame;
 
+import com.hackbulgaria.paint.view.PaintWindow;
+
+public class PaintController {
+	private final PaintWindow window;
+	private final Tool currentTool;
+
+	public PaintController() {
+		this.window = new PaintWindow();
+		window.setTitle("Paint!");
+		currentTool = Tool.DOT;
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setFocusable(true);
+		window.setVisible(true);
+		window.setSize(800, 600);
+	}
 }
