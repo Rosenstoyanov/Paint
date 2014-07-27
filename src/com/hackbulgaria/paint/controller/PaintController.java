a0527589b43bf3be29516d62959ab802371e7c4a
Milen <milengminev@gmail.com>
2014-07-28 01:12:29 +0300

    do commit

--- src/com/hackbulgaria/paint/controller/PaintController.java
+++ src/com/hackbulgaria/paint/controller/PaintController.java
@@ -1,17 +1,20 @@
 package com.hackbulgaria.paint.controller;
 
+import java.awt.Button;
 import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JFrame;
 
+import com.hackbulgaria.paint.sprites.BlankSprite;
 import com.hackbulgaria.paint.sprites.DotSprite;
+import com.hackbulgaria.paint.sprites.LineSprite;
 import com.hackbulgaria.paint.tools.Tool;
 import com.hackbulgaria.paint.view.PaintWindow;
 
 public class PaintController {
-	private final PaintWindow window;
+	private  PaintWindow window;
 	private final Tool currentTool;
 
 	public PaintController() {
@@ -27,14 +30,36 @@ public class PaintController {
 		// window.paint(g);
 		//window.setSize(800, 600);
 	}
-
+	
 	public class MouseListener extends MouseAdapter{
+		private int xStart;
+		private int yStart;
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			if(e.getButton() != 3){
+			System.out.println(String.format("%s %s %s %s", xStart, yStart, e.getX(), e.getY()));
+			if((e.getX() == xStart) && (e.getY() == yStart)){
+				DotSprite dot = new DotSprite(xStart, yStart, Color.green);
+				window.addSprite(dot);
+			}
+			else{
+				LineSprite line = new LineSprite(xStart, yStart, e.getX(), e.getY(), Color.green);
+				window.addSprite(line);
+			}
+			}
+			else{
+				BlankSprite blank = new BlankSprite();
+				window.addSprite(blank);
+			}
+			window.repaint();
+			super.mouseReleased(e);
+		}
+		
 		@Override
 		public void mousePressed(MouseEvent e) {
-			// System.out.println("Click!");
-			DotSprite dotSprite = new DotSprite(e.getX(), e.getY(), Color.white);
-			window.addSprite(dotSprite);
-			window.repaint();
+
+			xStart = e.getX();
+			yStart = e.getY();
 			super.mousePressed(e);
 		}
 
