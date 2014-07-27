85006775bf744df227b4111940aac3ad05793d5f
Milen <milengminev@gmail.com>
2014-07-27 21:56:29 +0300

    Added fields in Sprite class

--- src/com/hackbulgaria/paint/Main.java
+++ src/com/hackbulgaria/paint/Main.java
@@ -3,7 +3,6 @@ package com.hackbulgaria.paint;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
--- src/com/hackbulgaria/paint/sprites/Sprite.java
+++ src/com/hackbulgaria/paint/sprites/Sprite.java
@@ -1,5 +1,15 @@
 package com.hackbulgaria.paint.sprites;
 
+import java.awt.Color;
+
 public abstract class Sprite {
+	int x;
+	int y;  //position
+	int size;  // in pixels
+	Color color;
+	
+	public Sprite(){
+		this.color = Color.red;
+	}
 
 }
